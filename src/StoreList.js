7746d97d730996948748e78e4dbc794b1b21bf7a
paradiseMaestro <korotkih1liya@yandex.ru>
2020-08-05 11:04:00 +0300

    n

--- src/StoreList.js
+++ src/StoreList.js
@@ -1,24 +1,59 @@
 import React, { Component} from 'react';
 import axios from 'axios'
 
-
+async function u(){
+    fetch('http://178.128.196.163:3000/api/records', {
+      method: 'PUT',
+      body: JSON.stringify({
+          data: {
+              email: document.getElementById('email').value.toString(),
+              surname: document.getElementById('surname').value.toString(),
+              login: document.getElementById('login').value.toString(),
+              name: document.getElementById('name').value.toString(),
+          },
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    })
+    .then(response => response.json())
+}
 
 let isDisabled = [];
 class StoreList extends Component{
 
-    state = {
-        post:[]
+    constructor(){
+        super()
+        this.state = {
+            post:[],
+            active: false,
+        }
+        this.getData = this.getData.bind(this)
+        // this.handleClick = this.handleClick.bind(this);
+        // this.addActiveClass= this.addActiveClass.bind(this);
     }
+    // toggleClass() {
+    //     const currentState = this.state.active;
+    //     this.setState({ active: !currentState });
+    // };
+    lol = () => {
+        alert("fgh")
+    }
+    
 
     getData = () =>{
         axios.get('http://178.128.196.163:3000/api/records')
         .then(response => { 
             this.setState({
                 post : response.data,
+                btnState:true
               });
         });
     }
 
+    handleClick() {
+        return false;
+      }
 
     deleteItem = () => {
         fetch('http://178.128.196.163:3000/api/records/', {
@@ -41,9 +76,9 @@ class StoreList extends Component{
                         <li key={el._id} id={"elem"+i} className="store-item">
                             <div className="wrap-items">
                                 <input disabled={isDisabled[i]}  placeholder={el.data.login}  id={"login"+i} className="item-input item-input--save" type="text"/>
-                                <input disabled={isDisabled[i]} placeholder={el.data.surname} id={"surname"+i} className="item-input item-input--save" type="text"/>
-                                <input disabled={isDisabled[i]} placeholder={el.data.email} id={"email"+i} className="item-input item-input--save" type="text"/>
-                                <input disabled={isDisabled[i]} placeholder={el.data.name} id={"name"+i} className="item-input item-input--save" type="text"/>
+                                <input disabled={isDisabled[i]}  placeholder={el.data.surname} id={"surname"+i} className="item-input item-input--save" type="text"/>
+                                <input disabled={isDisabled[i]}  placeholder={el.data.email} id={"email"+i} className="item-input item-input--save" type="text"/>
+                                <input disabled={isDisabled[i]}  placeholder={el.data.name} id={"name"+i} className="item-input item-input--save" type="text"/>
                             </div>
                         <div onClick={
                             ()=>{
@@ -52,9 +87,39 @@ class StoreList extends Component{
                                 document.getElementById("surname"+i).classList.toggle('item-input--save')
                                 document.getElementById("email"+i).classList.toggle('item-input--save')
                                 document.getElementById("name"+i).classList.toggle('item-input--save')
-                                
+                                // this.setState({active: !this.state.active})
+                                // StoreList.getData()
+                                if(isDisabled[i]){
+                                    
+
+                                    fetch('http://178.128.196.163:3000/api/records/'+el._id, {
+                                        method: 'POST',
+                                        body: JSON.stringify({
+                                        data: {
+                                        email: document.getElementById("login"+i).value.toString(),
+                                        surname: document.getElementById("surname"+i).value.toString(),
+                                        login: document.getElementById("email"+i).value.toString(),
+                                        name: document.getElementById("name"+i).value.toString(),
+                                    },
+                                    }),
+
+                                    headers: {
+                                    "Content-type": "application/json; charset=UTF-8"
+                                    }
+
+                                    })
+
+
+
+
+
+
+
+                                }
                             }
-                        } className="btn btn--resave btn--save">
+                            // () => 
+                            // this.lol
+                        } className="btn-trig btn--resave btn--save">
                         {isDisabled[i] ? "edit":"save"}
                         
                         </div>
@@ -66,6 +131,7 @@ class StoreList extends Component{
                          }} className="btn--delete">
                         &#10006;
                         </div>
+                        <div className="lol">lol</div>
                         </li>
                         )
                     })
